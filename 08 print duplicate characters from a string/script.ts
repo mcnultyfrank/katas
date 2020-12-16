bf9304776d2b90a912c66cdf800a6adaf16b7f1e
Frank McNulty <frank.mcnulty@nology.io>
2020-12-16 15:22:38 +0000

    08 complete

--- 08 print duplicate characters from a string/script.ts
+++ 08 print duplicate characters from a string/script.ts
@@ -1,6 +1,11 @@
 // 08. How do you print duplicate characters from a string?
-let randomnamedefinitelyhasntbeusedbefore: string;
-randomnamedefinitelyhasntbeusedbefore = 'frank';
 
-console.log(randomnamedefinitelyhasntbeusedbefore);
+let sentence: string;
+sentence = 'hello there, you look settled. Try this apple.'
+const splitSentence = sentence.split("")
+console.log(splitSentence);
+
+for (let i = 0; i < splitSentence.length; i++) {
+    splitSentence[i] === splitSentence[i + 1] ? console.log(splitSentence[i], splitSentence[i + 1]) : null;
+}
 
