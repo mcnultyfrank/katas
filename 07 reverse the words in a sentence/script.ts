23eadfa9f29f78c249f29352587c0ae8731934b3
Frank McNulty <frank.mcnulty@nology.io>
2020-12-16 14:13:50 +0000

    07 complete

---
+++ 07 reverse the words in a sentence/script.ts
@@ -0,0 +1,10 @@
+// 07. How do you reverse the words in a sentence?
+
+var sentence: string;
+sentence = 'i will reverse this sentence';
+var reversedSentence = sentence.split("").reverse().join("");
+console.log(reversedSentence);
+
+
+
+
--- 07/script.ts
+++
@@ -1,5 +0,0 @@
-// 07. How do you find the missing numbers in a given integer array?
-// incomplete 
-
-
-
