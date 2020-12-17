009aceb55f11807723180e37d0358301f966d1bb
Frank McNulty <f-mcnulty@hotmail.com>
2020-12-17 11:02:18 +0000

    added logs

--- 09/script.ts
+++ 09/script.ts
@@ -2,8 +2,10 @@
 // 09. How do you check if two strings are anagrams of each other?
 let isItAnagram = function (firstword: string, secondword: string) {
     if (firstword.split("").sort().join("") === secondword.split("").sort().join("")){
+        console.log('they are anagrams');
         return true
     } else {
+        console.log('they are not anagrams');
         return false
     }
 };
